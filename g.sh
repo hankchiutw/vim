29b276caa54f9c1955ef8d43853ecbd85827b12c
hank <hank.chiu@kaiostech.com>
2017-11-08 09:25:56 +0800

    update g.sh

--- g.sh
+++ g.sh
@@ -2,15 +2,15 @@
 
 # grep file string under current location
 
-TARGET=$1
-
-grep "$TARGET" -nrI .\
+# ready to process full options user input
+# e.g. To show the nearby lines too: g.sh someString -C num
+grep "$@" -nrI .\
   --exclude=*.min.css\
   --exclude=*.min.js\
   --exclude=*.map\
   --exclude=*.svg\
-  --exclude=vendors.js \
-  --exclude=bundle.js \
+  --exclude=vendors.js\
+  --exclude=bundle.js\
   --exclude-dir=minify\
   --exclude-dir=node_modules\
   --exclude-dir=bower_components\
