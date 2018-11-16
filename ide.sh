892754ee722352a7c8c6c28dbb32f277b9d6f2b1
hank <hankchiu.tw@gmail.com>
2018-11-16 23:34:18 +0800

    set tmux window name

--- ide.sh
+++ ide.sh
@@ -2,14 +2,14 @@
 TARGET=${1:-.}
 cd $TARGET
 
-tmux new-session -d
+tmux new-session -d -n 'Editor'
 tmux splitw -h
 tmux splitw -v
 tmux select-pane -t 0
 tmux resize-pane -R 10
-tmux send-keys 'vi -c NERDTree' Enter
+tmux send-keys 'vim -c NERDTree' Enter
 
-tmux new-window
+tmux new-window -n 'Console'
 tmux select-window -t 0
 
 tmux attach-session
