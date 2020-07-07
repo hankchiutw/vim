cb133ef342036936b9a13d5d5d01e49c055175d1
hank <hankchiu.tw@gmail.com>
2020-07-07 23:18:33 +0800

    chore(eslint): improve .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,31 +14,37 @@ module.exports = {
     browser: true,
     commonjs: true,
     node: true,
-    es6: true,
+    es6: true
   },
   plugins: ['@typescript-eslint', 'import'],
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:react/recommended',
+    'plugin:react/recommended'
   ],
   overrides: [
     {
       files: ['*.js*', '*.ts*'],
       rules: {
         '@typescript-eslint/explicit-function-return-type': 'off',
-      },
-    },
+        '@typescript-eslint/no-empty-function': 'off',
+        '@typescript-eslint/no-unused-vars': [
+          'warn',
+          { argsIgnorePattern: '^_' }
+        ]
+      }
+    }
   ],
   rules: {
     'comma-dangle': ['warn', 'always-multiline'],
     semi: ['warn', 'always'],
+    'no-console': ['warn'],
     'import/order': [
       'warn',
       {
-        alphabetize: { order: 'asc', caseInsensitive: true },
-      },
-    ],
-  },
+        alphabetize: { order: 'asc', caseInsensitive: true }
+      }
+    ]
+  }
 };
