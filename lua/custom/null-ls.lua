fdfcaae0d1f887a5cbc8c31bb184175f811a237e
hank <hankchiu.tw@gmail.com>
2023-01-09 23:25:02 +0800

    feat(vim/null-ls): use eslint_d

--- lua/custom/null-ls.lua
+++ lua/custom/null-ls.lua
@@ -6,11 +6,11 @@ local function gen_sources()
     null_ls.builtins.formatting.stylua,
     null_ls.builtins.formatting.black,
     null_ls.builtins.formatting.isort,
-    null_ls.builtins.diagnostics.eslint,
+    null_ls.builtins.diagnostics.eslint_d,
     null_ls.builtins.diagnostics.pylint,
     null_ls.builtins.diagnostics.flake8,
     -- null_ls.builtins.diagnostics.tsc,
-    null_ls.builtins.formatting.prettierd,
+    null_ls.builtins.formatting.eslint_d,
   }
 end
 
