99c18cce1b6c74839434be53c972adb434542e48
hank <hankchiu.tw@gmail.com>
2023-07-19 18:13:43 +0800

    feat(vim/diffview): able to show the latest commit diff of current line

--- lua/custom/diffview.lua
+++ lua/custom/diffview.lua
@@ -14,15 +14,50 @@ local function make_toggle_fn(fn)
   end
 end
 
+local function blame_commit_info()
+  local file_path = vim.fn.expand("%:p")
+  local line_num = vim.fn.line(".")
+  local cmd = "git blame -p -L" .. line_num .. ",+1 " .. file_path
+  local rows = vim.fn.split(vim.fn.system(cmd), "\n")
+  local commit_rev = vim.fn.split(table.remove(rows, 1), " ")[1]
+
+  local commit_info = { sha = string.sub(commit_rev, 1, 10) }
+  for _, row in ipairs(rows) do
+    local ar = vim.fn.split(row, " ")
+    local k = table.remove(ar, 1)
+    local v = table.concat(ar, " ")
+    if k == "author" then
+      commit_info[k] = v
+    elseif k == "summary" then
+      commit_info[k] = v
+    elseif k == "author-time" then
+      commit_info.date = os.date("%Y-%m-%d %X", tonumber(v))
+    end
+  end
+
+  return commit_info
+end
+
 local toggle_open = make_toggle_fn(diffview.open)
 local toggle_file_history = make_toggle_fn(function()
   diffview.file_history(nil, "%")
 end)
 
+local toggle_line_blame = make_toggle_fn(function()
+  local commit_info = blame_commit_info()
+  local sha = commit_info.sha
+
+  diffview.open(sha .. "~1.." .. sha)
+
+  local info = table.concat({ sha, commit_info.date, commit_info.author, commit_info.summary }, " • ")
+  print(info)
+end)
+
 local function set_keymap()
   local flag = { silent = true, noremap = true }
-  vim.keymap.set("n", ",d", toggle_open, flag)
+  vim.keymap.set("n", ",t", toggle_open, flag)
   vim.keymap.set("n", ",f", toggle_file_history, flag)
+  vim.keymap.set("n", ",d", toggle_line_blame, flag)
 end
 
 function M.setup()
