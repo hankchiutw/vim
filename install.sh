414c069b2c779dfc83725de544b78d8165db5371
hank <hankchiu.tw@gmail.com>
2024-02-23 15:25:36 +0800

    feat(git): enable delta

--- install.sh
+++ install.sh
@@ -60,7 +60,7 @@ apt-get install -y exuberant-ctags cmake python-dev python3-dev screen gawk colo
 export HOMEBREW_NO_AUTO_UPDATE=1
 
 brew update
-brew install python3 automake gawk colordiff tmux the_silver_searcher tig
+brew install python3 automake gawk colordiff tmux the_silver_searcher tig git-delta
 # ctags
 brew install --HEAD universal-ctags/universal-ctags/universal-ctags
 
