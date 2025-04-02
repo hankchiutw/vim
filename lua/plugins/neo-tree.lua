0c1873006835d482475a23aa3d36e244326d6a07
hank <hankchiu.tw@gmail.com>
2025-04-02 23:18:39 +0800

    feat(lua/neo-tree): set keymap

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,3 +1,9 @@
+local function set_keymap()
+  local flag = { silent = true, noremap = true }
+  vim.keymap.set("n", "<leader>e", ":Neotree<cr>", flag)
+  vim.keymap.set("v", "<leader>e", ":Neotree<cr>", flag)
+end
+
 return {
   "nvim-neo-tree/neo-tree.nvim",
   branch = "v3.x",
@@ -8,9 +14,19 @@ return {
     -- {"3rd/image.nvim", opts = {}}, -- Optional image support in preview window: See `# Preview Mode` for more information
   },
   lazy = false, -- neo-tree will lazily load itself
+  init = function()
+    set_keymap()
+  end,
   ---@module "neo-tree"
   ---@type neotree.Config?
   opts = {
-    -- fill any relevant options here
+    window = {
+      position = "right",
+      mappings = {
+        ["l"] = "open",
+        ["h"] = "close_node",
+        ["<space>"] = "focus_preview",
+      },
+    },
   },
 }
