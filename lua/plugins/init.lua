5e48dddaaf4a0a8b73bb58682f1f01b2d2eba3bb
hank <hankchiu.tw@gmail.com>
2025-04-22 00:26:03 +0800

    feat: add vim-fugitive with key mappings for git operations
    - Added vim-fugitive plugin for enhanced git integration
    - Configured keybinding <c-g>b for Git blame view
    - Added keybinding <c-g>f to show file history in git
    - Set up mappings in normal mode with descriptive labels

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -3,4 +3,11 @@ return {
     "windwp/nvim-autopairs",
     opts = {},
   },
+  {
+    "tpope/vim-fugitive",
+    init = function()
+      vim.keymap.set("n", "<c-g>b", ":G blame<CR>", { desc = "Git blame" })
+      vim.keymap.set("n", "<c-g>f", ":G file %<CR><c-w>_", { desc = "Git file history" })
+    end,
+  },
 }
