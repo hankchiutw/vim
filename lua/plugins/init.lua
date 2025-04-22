cd98ae8076935826c4917cc8d8b87b68223fc03d
hank <hankchiu.tw@gmail.com>
2025-04-22 12:53:29 +0800

    feat(keymaps): add command-line and git reset keybindings
    - Added <C-k> and <C-j> keymaps in command-line mode to navigate command history up and down
    - Introduced <c-g>r normal mode keymap to reset current file in Git to HEAD using :G co %
    - Updated plugin initialization to include new git reset keymap with descriptive label

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,3 +1,6 @@
+vim.keymap.set("c", "<C-k>", "<Up>")
+vim.keymap.set("c", "<C-j>", "<Down>")
+
 return {
   {
     "windwp/nvim-autopairs",
@@ -8,6 +11,7 @@ return {
     init = function()
       vim.keymap.set("n", "<c-g>b", ":G blame<CR>", { desc = "Git blame" })
       vim.keymap.set("n", "<c-g>f", ":G file %<CR><c-w>_", { desc = "Git file history" })
+      vim.keymap.set("n", "<c-g>r", ":G co %<CR>", { desc = "Git reset HEAD" })
     end,
   },
 }
