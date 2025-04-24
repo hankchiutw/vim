181097a730705c83a59ce7203497a3efd71d1405
hank <hankchiu.tw@gmail.com>
2025-04-24 15:50:31 +0800

    feat(keymap): add Alt+o and Alt+i shortcuts for cursor navigation in normal mode
    - Mapped <A-o> (Alt+o) in normal mode to function like <C-o>, jumping to older cursor positions in the jumplist.
    - Mapped <A-i> (Alt+i) in normal mode to function like <C-i>, jumping to newer cursor positions in the jumplist.
    - Both mappings are set as non-recursive to prevent unintended remapping behavior.
    - Enhances navigation flexibility by providing alternative shortcuts for cursor history traversal.

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,6 +1,11 @@
 vim.keymap.set("c", "<C-k>", "<Up>")
 vim.keymap.set("c", "<C-j>", "<Down>")
 
+-- Map Alt+o in normal mode to jump to older cursor position (like <C-o>)
+vim.keymap.set("n", "<A-o>", "<C-o>", { noremap = true })
+-- Map Alt+i in normal mode to jump to newer cursor position (like <C-i>)
+vim.keymap.set("n", "<A-i>", "<C-i>", { noremap = true })
+
 return {
   {
     "windwp/nvim-autopairs",
