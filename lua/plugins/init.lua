6ddf762f715317d1f3d5e8618bc91da8e75169f4
hank <hankchiu.tw@gmail.com>
2025-06-04 22:54:39 +0800

    feat: enhance git integration and terminal handling
    - Updated `.gitconfig` to use `delta` as pager for blame, diff, reflog, show, and log commands, improving Git output readability.
    - Added Neovim autocommand to automatically close terminal buffers when the shell exits successfully, reducing manual cleanup.
    - Adjusted Fugitive mapping for "Git file history" to ensure window resizes correctly after invoking the command.

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -6,6 +6,18 @@ vim.keymap.set("n", "<A-o>", "<C-o>", { noremap = true })
 -- Map Alt+i in normal mode to jump to newer cursor position (like <C-i>)
 vim.keymap.set("n", "<A-i>", "<C-i>", { noremap = true })
 
+-- Automatically close terminal buffer if the shell exited successfully
+vim.api.nvim_create_autocmd("TermClose", {
+  pattern = "*",
+  callback = function(args)
+    vim.schedule(function()
+      if vim.bo.buftype == "terminal" and vim.v.shell_error == 0 then
+        vim.cmd("bdelete! " .. args.buf)
+      end
+    end)
+  end,
+})
+
 return {
   {
     "windwp/nvim-autopairs",
@@ -15,7 +27,7 @@ return {
     "tpope/vim-fugitive",
     init = function()
       vim.keymap.set("n", "<c-g>b", ":G blame<CR>", { desc = "Git blame" })
-      vim.keymap.set("n", "<c-g>f", ":G file %<CR><c-w>_", { desc = "Git file history" })
+      vim.keymap.set("n", "<c-g>f", ":G file %<CR><c-\\><c-o><c-w>_", { desc = "Git file history" })
       vim.keymap.set("n", "<c-g>r", ":G co %<CR>", { desc = "Git reset HEAD" })
     end,
   },
