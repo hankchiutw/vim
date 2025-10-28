8330b21b8a48b1969dede91d56675b2611a5ec93
hank <hankchiu.tw@gmail.com>
2025-10-28 15:13:43 +0800

    chore(plugins): remove NERDCommenter and remap <leader>c to gc/gcc
    - Add normal-mode mapping: <leader>c -> gcc (remap) with description "Toggle comment line"
    - Add visual-mode mapping: <leader>c -> gc (remap) with description "Toggle comment selection"
    - Remove lua/plugins/nerdcommenter.lua and its NERDCommenter settings (NERDSpaceDelims, NERDDefaultAlign)
    
    BREAKING CHANGE: NERDCommenter plugin removed. Comment toggling now expects a plugin that provides the gc/gcc mappings (e.g. Comment.nvim or vim-commentary). Update your plugin list if you relied on NERDCommenter.

--- lua/plugins/custom/keymap.lua
+++ lua/plugins/custom/keymap.lua
@@ -8,6 +8,10 @@ function M.set()
   vim.keymap.set("n", "<A-o>", "<C-o>", { noremap = true })
   -- Map Alt+i in normal mode to jump to newer cursor position (like <C-i>)
   vim.keymap.set("n", "<A-i>", "<C-i>", { noremap = true })
+
+  -- Remap in Normal mode for toggling comment on a line and selection
+  vim.keymap.set("n", "<leader>c", "gcc", { remap = true, desc = "Toggle comment line" })
+  vim.keymap.set("v", "<leader>c", "gc", { remap = true, desc = "Toggle comment selection" })
 end
 
 return M
