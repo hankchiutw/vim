7fff13e2534f1cf22fe6d77718824dfbf5d3707f
hank <hankchiu.tw@gmail.com>
2025-11-24 16:11:20 +0800

    feat(keymap): add Command+o and Command+i mappings for cursor position jump on mac
    - Added <D-o> and <D-i> keymaps in normal mode to jump to older/newer cursor positions, making navigation consistent for macOS users.
    - Retained existing <A-o> and <A-i> mappings for other platforms.

--- lua/plugins/custom/keymap.lua
+++ lua/plugins/custom/keymap.lua
@@ -4,10 +4,12 @@ function M.set()
   vim.keymap.set("c", "<C-k>", "<Up>")
   vim.keymap.set("c", "<C-j>", "<Down>")
 
-  -- Map Alt+o in normal mode to jump to older cursor position (like <C-o>)
+  -- Map Alt+o(Command+o for mac) in normal mode to jump to older cursor position (like <C-o>)
   vim.keymap.set("n", "<A-o>", "<C-o>", { noremap = true })
-  -- Map Alt+i in normal mode to jump to newer cursor position (like <C-i>)
+  vim.keymap.set("n", "<D-o>", "<C-o>", { noremap = true })
+  -- Map Alt+i(Command+i for mac) in normal mode to jump to newer cursor position (like <C-i>)
   vim.keymap.set("n", "<A-i>", "<C-i>", { noremap = true })
+  vim.keymap.set("n", "<D-i>", "<C-i>", { noremap = true })
 
   -- Remap in Normal mode for toggling comment on a line and selection
   vim.keymap.set("n", "<leader>c", "gcc", { remap = true, desc = "Toggle comment line" })
